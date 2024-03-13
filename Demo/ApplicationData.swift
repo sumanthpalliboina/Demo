ff5ac9aa45062142fa0117074309c3911230a282
sumanthpalliboina <palleboinasumanth@gmail.com>
2024-03-13 13:33:01 +0530

    search books in list of books Implemented

---
+++ Demo/ApplicationData.swift
@@ -0,0 +1,8 @@
+//
+//  ApplicationData.swift
+//  Demo
+//
+//  Created by Palliboina on 13/03/24.
+//
+
+import Foundation
---
+++ Demo/SearchListView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchListView.swift
+//  Demo
+//
+//  Created by Palliboina on 13/03/24.
+//
+
+import SwiftUI
+
+struct SearchListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchListView()
+}
